1a2da0cf6c216e2ef6c3530e836686467a0bcf4d
mannyjimen <manueljimenez0320@gmail.com>
2026-06-03 19:09:42 -0400

    a little more skeleton adding, and trying to get the organization done for data scraping

---
+++ main.go
@@ -0,0 +1,7 @@
+package main
+
+import "github.com/mannyjimen/hospitality-analyzer/reviewfilter"
+
+func main() {
+	business_ids := reviewfilter.GetNegativeBusinessIDs()
+}
--- reviewfilter/filter.go
+++ reviewfilter/filter.go
@@ -2,10 +2,30 @@ package reviewfilter
 
 import (
 	"bufio"
+	"log"
 	"os"
 )
+
+// map containing negative keywords, will be used for review filtering.
+// All negative keywords are newline separated and are in './negativekeywords.txt'
+var keywords = make(map[string]bool)
 
 type ReviewStreamer struct {
 	file    *os.File
 	scanner *bufio.Scanner
+}
+
+func GetNegativeBusinessIDs() []string {
+	file, err := os.Open("negativekeywords.txt")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+	streamer := ReviewStreamer{
+		file:    file,
+		scanner: scanner}
+
+	return []string{}
 }
