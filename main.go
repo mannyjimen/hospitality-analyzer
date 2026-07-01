e44f4a115c250d69a4569a14a260593861f13c82
mannyjimen <manueljimenez0320@gmail.com>
2026-07-01 16:16:54 -0400

    reveiwfilter is done, storing all business_ids with at least one unfair review (review that contains negative keyword) as well as assuring the review is in one of the selected cities. However, it is taking a bit long for my liking. I am going to try to optimize it, since its around a 25 second runtime to get all the business_ids.

--- main.go
+++ main.go
@@ -1,8 +1,14 @@
 package main
 
-import "github.com/mannyjimen/hospitality-analyzer/reviewfilter"
+import (
+	"github.com/mannyjimen/hospitality-analyzer/reviewfilter"
+)
 
 func main() {
 	business_ids := reviewfilter.GetNegativeBusinessIDs()
 	_ = business_ids
+
+	// for _, id := range business_ids {
+	// 	fmt.Println(id)
+	// }
 }
--- reviewfilter/filter.go
+++ reviewfilter/filter.go
@@ -2,12 +2,15 @@ package reviewfilter
 
 import (
 	"bufio"
+	"encoding/json"
+	"log"
 	"os"
+	"strings"
 )
 
 type Review struct {
-	business_id string
-	text        string
+	Business_id string `json:"business_id"`
+	Text        string `json:"text"`
 }
 
 type ReviewStreamer struct {
@@ -18,10 +21,72 @@ type ReviewStreamer struct {
 func GetNegativeBusinessIDs() []string {
 
 	preprocess()
-	// scanner := bufio.NewScanner(file)
-	// streamer := ReviewStreamer{
-	// 	file:    file,
-	// 	scanner: scanner}
+	streamer := getReviewStreamer("YelpJSON/yelp_academic_dataset_review.json")
+	ids := getNegativeBusinessIDs(streamer)
 
-	return []string{}
+	return ids
+}
+
+func getReviewStreamer(filePath string) ReviewStreamer {
+	file, err := os.Open(filePath)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	scanner := bufio.NewScanner(file)
+	streamer := ReviewStreamer{
+		file:    file,
+		scanner: scanner}
+
+	return streamer
+}
+
+//go through every review
+//first determine whether the review is of a business with a bussiness_id in businesses map
+//then determine whether review text has any negative keyword
+//if so, we need to store/remember the business_id of this "unfair treatement" review
+
+func getNegativeBusinessIDs(streamer ReviewStreamer) []string {
+	var unfairBusinessIDs = make(map[string]struct{})
+	for streamer.scanner.Scan() {
+		review := streamer.getNextReview()
+
+		if isSelectedBusiness(review.Business_id) && isUnfairReview(review.Text) {
+			unfairBusinessIDs[review.Business_id] = struct{}{}
+		}
+	}
+
+	var idList = []string{}
+	for id := range unfairBusinessIDs {
+		idList = append(idList, id)
+	}
+	return idList
+}
+
+func (r ReviewStreamer) getNextReview() Review {
+	var review Review
+
+	err := json.Unmarshal([]byte(r.scanner.Text()), &review)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	return review
+}
+
+// returns whether the business_id is in businesses map (of chosen cities)
+func isSelectedBusiness(business_id string) bool {
+	_, ok := businesses[business_id]
+	return ok
+}
+
+// returns whether a negative keyword in the review text
+func isUnfairReview(review_text string) bool {
+	for keyword := range keywords {
+		if strings.Contains(review_text, keyword) {
+			return true
+		}
+	}
+	return false
 }
